c4a5e12def031c6cb18f746b971f40e0a891303d
Benjamin Morales <129797391+RyuDakai@users.noreply.github.com>
2024-12-13 09:35:01 -0300

    update login screen employee

--- lib/presentation/screen/loginScreen.dart
+++ lib/presentation/screen/loginScreen.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:enviaya/presentation/screen/homeEmployee.dart';
 import 'package:enviaya/presentation/screen/homeAssing.dart';
+import 'package:enviaya/presentation/screen/registerScreen.dart'; // Pantalla de registro
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -14,15 +15,14 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   bool _isPasswordVisible = false;
 
-  // Controladores para capturar el correo y la contraseña
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _adminPasswordController = TextEditingController(); // Controlador para la contraseña de administrador
 
-  // Instancia de FirebaseAuth y Firestore
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  // Método para iniciar sesión con Firebase
+  // Método para iniciar sesión
   Future<void> _signInWithEmailAndPassword() async {
     try {
       final UserCredential userCredential =
@@ -31,7 +31,6 @@ class _LoginScreenState extends State<LoginScreen> {
         password: _passwordController.text.trim(),
       );
 
-      // Obtener datos del usuario desde Firestore
       final DocumentSnapshot userDoc = await _firestore
           .collection('users')
           .doc(userCredential.user?.uid)
@@ -39,8 +38,6 @@ class _LoginScreenState extends State<LoginScreen> {
 
       if (userDoc.exists) {
         final userData = userDoc.data() as Map<String, dynamic>;
-
-        // Verificar el rol del usuario
         final String userRole = userData['role'] ?? '';
 
         if (userRole == 'Repartidor') {
@@ -79,7 +76,36 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
-  // Método para mostrar mensajes
+  // Método para verificar la contraseña de administrador
+  Future<void> _verifyAdminPassword() async {
+    try {
+      // Consulta todos los documentos en la colección "admin_codes"
+      final QuerySnapshot querySnapshot =
+          await _firestore.collection('admin_codes').get();
+
+      // Buscar si existe un documento con el adminCode igual al ingresado
+      final bool adminCodeExists = querySnapshot.docs.any((doc) {
+        final adminData = doc.data() as Map<String, dynamic>;
+        return adminData['adminCode'] == _adminPasswordController.text.trim();
+      });
+
+      if (adminCodeExists) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  const RegisterScreen()), // Redirige a la pantalla de registro
+        );
+      } else {
+        _showMessage("Contraseña de administrador incorrecta.");
+      }
+    } catch (e) {
+      _showMessage("Error al verificar la contraseña: ${e.toString()}");
+    }
+  }
+
+
+
   void _showMessage(String message) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
@@ -181,11 +207,64 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                 ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () => _showAdminPasswordDialog(),
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      backgroundColor: Colors.grey[800],
+                    ),
+                    child: const Text(
+                      "Registrar nuevo usuario",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  // Mostrar diálogo para ingresar la contraseña de administrador
+  void _showAdminPasswordDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Contraseña de administrador"),
+          content: TextField(
+            controller: _adminPasswordController,
+            obscureText: true,
+            decoration: const InputDecoration(
+              labelText: "Contraseña",
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text("Cancelar"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _verifyAdminPassword();
+              },
+              child: const Text("Verificar"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
