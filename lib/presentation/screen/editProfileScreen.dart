aa2263806da195a7370e266fad7da12ec6896403
Benjamin Morales <129797391+RyuDakai@users.noreply.github.com>
2024-12-18 17:09:28 -0300

    Final Version?

--- lib/presentation/screen/editProfileScreen.dart
+++ lib/presentation/screen/editProfileScreen.dart
@@ -1,243 +1,154 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
 class EditProfileScreen extends StatefulWidget {
   const EditProfileScreen({super.key});
-
   @override
-  _EditProfileScreenState createState() => _EditProfileScreenState();
+  State<EditProfileScreen> createState() => _EditProfileScreenState();
 }
-
 class _EditProfileScreenState extends State<EditProfileScreen> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  // Controladores para los campos
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _addressController = TextEditingController();
-  final TextEditingController _cityController = TextEditingController();
-  final TextEditingController _postalCodeController = TextEditingController();
-  final TextEditingController _phoneController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  String _rut = ''; // El RUT será de solo lectura
-
+  final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _addressController = TextEditingController();
+  final _cityController = TextEditingController();
+  final _postalCodeController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _rutController = TextEditingController();
+  bool _isLoading = true;
   @override
   void initState() {
     super.initState();
     _loadUserData();
   }
-
-  // Cargar datos del usuario desde Firestore
+  // Cargar los datos del usuario desde Firestore
   Future<void> _loadUserData() async {
-    final user = _auth.currentUser;
-
-    if (user != null) {
-      final DocumentSnapshot userDoc =
-          await _firestore.collection('clients').doc(user.uid).get();
-
-      if (userDoc.exists) {
-        final userData = userDoc.data() as Map<String, dynamic>;
-
-        setState(() {
-          _nameController.text = userData['name'] ?? '';
-          _emailController.text = userData['email'] ?? '';
-          _addressController.text = userData['address'] ?? '';
-          _cityController.text = userData['city'] ?? '';
-          _postalCodeController.text = userData['postal_code'] ?? '';
-          _phoneController.text = userData['phone'] ?? '';
-          _rut = userData['rut'] ?? ''; // RUT solo lectura
-        });
-      }
-    }
-  }
-
-  // Actualizar los datos del usuario
-  Future<void> _updateUserData() async {
-    final user = _auth.currentUser;
-
-    if (user == null) return;
-
-    // Validar campos requeridos
-    if (_nameController.text.trim().isEmpty ||
-        _emailController.text.trim().isEmpty ||
-        _passwordController.text.trim().isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("Por favor, completa todos los campos.")),
-      );
-      return;
-    }
-
     try {
-      // Reautenticación del usuario
-      final AuthCredential credential = EmailAuthProvider.credential(
-        email: user.email!,
-        password: _passwordController.text.trim(),
-      );
-
-      await user.reauthenticateWithCredential(credential);
-
-      // Si el correo ha cambiado, actualizarlo en Firebase Auth
-      if (_emailController.text.trim() != user.email) {
-        await user.updateEmail(_emailController.text.trim());
+      final user = _auth.currentUser;
+      if (user == null) return;
+      final docSnapshot =
+          await _firestore.collection('clients').doc(user.uid).get();
+      if (docSnapshot.exists) {
+        final data = docSnapshot.data() as Map<String, dynamic>;
+        _nameController.text = data['name'] ?? '';
+        _emailController.text = data['email'] ?? '';
+        _addressController.text = data['address'] ?? '';
+        _cityController.text = data['city'] ?? '';
+        _postalCodeController.text = data['postal_code'] ?? '';
+        _phoneController.text = data['phone'] ?? '';
+        _rutController.text = data['rut'] ?? ''; // Cargar RUT pero no editable
       }
-
-      // Actualizar datos en Firestore (sin modificar el RUT)
-      await _firestore.collection('clients').doc(user.uid).update({
-        'name': _nameController.text.trim(),
-        'email': _emailController.text.trim(),
-        'address': _addressController.text.trim(),
-        'city': _cityController.text.trim(),
-        'postal_code': _postalCodeController.text.trim(),
-        'phone': _phoneController.text.trim(),
-      });
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("Datos actualizados con éxito.")),
-      );
-
-      _passwordController.clear(); // Limpiar el campo de la contraseña
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Error al actualizar los datos: $e")),
+        SnackBar(content: Text("Error al cargar datos: $e")),
       );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
     }
   }
-
+    // Actualizar datos del usuario
+Future<void> _updateUserData() async {
+  final user = _auth.currentUser;
+  if (user == null) return;
+  // Validar campos requeridos
+  if (_nameController.text.trim().isEmpty ||
+      _emailController.text.trim().isEmpty) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text("Por favor, completa todos los campos.")),
+    );
+    return;
+  }
+  try {
+    // Verificar si el correo electrónico ha cambiado
+    if (_emailController.text.trim() != user.email) {
+      await user.verifyBeforeUpdateEmail(_emailController.text.trim());
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content: Text(
+                "Se envió un correo de verificación. Verifica antes de continuar.")),
+      );
+    }
+    // Actualizar datos en Firestore (sin modificar el RUT)
+    await _firestore.collection('clients').doc(user.uid).update({
+      'name': _nameController.text.trim(),
+      'email': _emailController.text.trim(),
+      'address': _addressController.text.trim(),
+      'city': _cityController.text.trim(),
+      'postal_code': _postalCodeController.text.trim(),
+      'phone': _phoneController.text.trim(),
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text("Datos actualizados con éxito.")),
+    );
+  } catch (e) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("Error al actualizar los datos: $e")),
+    );
+  }
+}
+  // Widget para los campos de texto
+  Widget _buildTextField(String label, TextEditingController controller,
+      {bool readOnly = false}) {
+    return TextField(
+      controller: controller,
+      readOnly: readOnly,
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Editar Perfil"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Campo: Nombre
-              TextField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  labelText: "Nombre",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildTextField("Nombre", _nameController),
+                  const SizedBox(height: 10),
+                  _buildTextField("Correo Electrónico", _emailController),
+                  const SizedBox(height: 10),
+                  _buildTextField("Dirección", _addressController),
+                  const SizedBox(height: 10),
+                  _buildTextField("Ciudad", _cityController),
+                  const SizedBox(height: 10),
+                  _buildTextField("Código Postal", _postalCodeController),
+                  const SizedBox(height: 10),
+                  _buildTextField("Teléfono", _phoneController),
+                  const SizedBox(height: 10),
+                  _buildTextField(
+                    "RUT",
+                    _rutController,
+                    readOnly: true, // RUT no editable
                   ),
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Campo: Correo Electrónico
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: "Correo Electrónico",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Campo: Dirección
-              TextField(
-                controller: _addressController,
-                decoration: InputDecoration(
-                  labelText: "Dirección",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Campo: Ciudad
-              TextField(
-                controller: _cityController,
-                decoration: InputDecoration(
-                  labelText: "Ciudad",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Campo: Código Postal
-              TextField(
-                controller: _postalCodeController,
-                decoration: InputDecoration(
-                  labelText: "Código Postal",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Campo: Teléfono
-              TextField(
-                controller: _phoneController,
-                decoration: InputDecoration(
-                  labelText: "Teléfono",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Campo: RUT (Solo lectura)
-              TextField(
-                controller: TextEditingController(text: _rut),
-                enabled: false,
-                decoration: InputDecoration(
-                  labelText: "RUT",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Campo: Contraseña actual (para reautenticación)
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Contraseña Actual",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 24),
-
-              // Botón para guardar cambios
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: _updateUserData,
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    backgroundColor: Colors.black,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: _updateUserData,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      ),
+                      child: const Text(
+                        "Guardar Cambios",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
                     ),
                   ),
-                  child: const Text(
-                    "Guardar Cambios",
-                    style: TextStyle(fontSize: 16, color: Colors.white),
-                  ),
-                ),
+                ],
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
